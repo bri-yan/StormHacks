39c3477a3db65c5b4cd21b8dcb82a161a3971e56
bri-yan <bri.yan@outlook.com>
2021-02-20 19:25:28 -0800

    edited style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button, scrollTo } from "react-atomize";
 
 export class Header extends Component {
   render() {
@@ -17,13 +18,23 @@ export class Header extends Component {
                   <p>
                     {this.props.data ? this.props.data.paragraph : "Loading"}
                   </p>
-                  <a
-                    href="#features"
+                  <div className="center-button">
+                    <Button
+                      h="3.5rem"
+                      w="10rem"
+                      p={{ x: "1.25rem" }}
+                      textSize="body"
+                      hoverTextColor="light"
+                      hoverBg="dark"
+                      m={{ r: "0.5rem" }}
                     
-                    className="btn btn-custom btn-lg page-scroll"  
-                  >
-                    Try It Out
-                  </a>{" "}
+                      rounded="circle"
+                      onClick={() => scrollTo('#features')}
+                    >
+                      TRY IT OUT
+                    </Button>
+                  </div>
+                  
                 </div>
               </div>
             </div>
