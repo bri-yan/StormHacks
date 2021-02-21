ed5211b9789efad605f5d5ab8cf289054f7c4ec2
bri-yan <bri.yan@outlook.com>
2021-02-20 17:52:32 -0800

    added searchbar and card components

---
+++ src/components/Articles.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Div, Row, Col } from "react-atomize";
+import Card from "./Card";
+import LongCard from "./LongCard"
+
+function Articles() {
+  return (
+    <Div>
+    <Row size="auto" d="flex" p="0rem">
+      <Col size="auto" p="0rem">
+        <Card
+          title="Lorem ipsum dolor sit amet, conse tetur adipiscing elit"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+          img="https://images.unsplash.com/photo-1561212024-cb9ad0c33195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=2199&h=594&q=80"
+        />  
+      </Col>
+      <Col size="auto">
+        <Card
+          title="Lorem ipsum dolor sit amet, conse tetur adipiscing elit"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+          img="https://images.unsplash.com/photo-1561212024-cb9ad0c33195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=2199&h=594&q=80"
+        />  
+      </Col>
+      <Col size="auto">
+        <Card
+          title="Lorem ipsum dolor sit amet, conse tetur adipiscing elit"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+          img="https://images.unsplash.com/photo-1561212024-cb9ad0c33195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=2199&h=594&q=80"
+        />  
+      </Col>
+      <Col size="auto">
+        <Card
+          title="Lorem ipsum dolor sit amet, conse tetur adipiscing elit"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+          img="https://images.unsplash.com/photo-1561212024-cb9ad0c33195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=2199&h=594&q=80"
+        />  
+      </Col>
+    </Row>
+    <Row size="auto" d="flex" p="0rem">
+      <Col size="auto" p="0rem">
+        <LongCard
+          title="Lorem ipsum dolor sit amet, conse tetur adipiscing elit"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+          img="https://images.unsplash.com/photo-1561212024-cb9ad0c33195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=2199&h=594&q=80"
+        />  
+      </Col>
+      <Col size="auto">
+        <LongCard
+          title="Lorem ipsum dolor sit amet, conse tetur adipiscing elit"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+          img="https://images.unsplash.com/photo-1561212024-cb9ad0c33195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=2199&h=594&q=80"
+        />  
+      </Col>
+    </Row>
+    </Div>
+  );
+}
+
+export default Articles;
---
+++ src/components/Searchbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Input, Button, Icon, Div } from "react-atomize";
+
+class SearchLoading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      value: ''
+    };
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+        <Div
+            w="60.4rem"
+            p={{l:"2rem", r:"2rem", b:"2rem", t:"3rem"}}
+        >
+            <Input
+            bg="white"
+            textWeight="500"
+            placeholder="Search"
+            rounded="circle"
+            onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                    console.log(e.target.value);
+                    this.setState({ isLoading: false });
+                } else {
+                    this.setState({ isLoading: true });
+                }
+            }}
+            suffix={
+            <Button
+                pos="absolute"
+                onClick={(e) => {
+                        this.setState({ isLoading: !isLoading });
+                        console.log(Input.e)
+                    }
+                }
+                bg="transparent"
+                w="3rem"
+                top="0"
+                right="0"
+                rounded={{ r: "md" }}
+            >
+                <Icon
+                name={isLoading ? "Loading" : "Search"}
+                color={isLoading ? "gray900" : "black"}
+                size="16px"
+                />
+            </Button>
+            }
+            />
+        </Div>
+    );
+  }
+}
+export default SearchLoading;
